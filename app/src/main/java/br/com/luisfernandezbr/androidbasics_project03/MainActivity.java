3aa61448733ccc68e74d980217eca510faceaa11
Luis E. Fernandez <luis82fernandez@gmail.com>
2017-02-11 23:50:45 -0200

    Project created

---
+++ app/src/main/java/br/com/luisfernandezbr/androidbasics_project03/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.luisfernandezbr.androidbasics_project03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
