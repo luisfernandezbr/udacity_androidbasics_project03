b42ab2f21320682bc7957892581c6f14aade3714
Luis E. Fernandez <luis82fernandez@gmail.com>
2017-02-25 23:02:14 -0300

    Creating MultipleOptionQuestion

---
+++ app/src/main/java/br/com/luisfernandezbr/androidbasics_project03/pojo/MultipleOptionQuestion.java
@@ -0,0 +1,52 @@
+package br.com.luisfernandezbr.androidbasics_project03.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by luis.fernandez on 2/12/17.
+ */
+
+public class MultipleOptionQuestion extends Question {
+
+    public static final String TYPE = "MULTIPLE_OPTION";
+
+    private Map<Integer, Answer> answerMap;
+    private List<Integer> userAnswerList;
+
+    public MultipleOptionQuestion(String value, Map<Integer, Answer> answerMap) {
+        super(value, TYPE);
+        this.answerMap = answerMap;
+        this.userAnswerList = new ArrayList<>(answerMap.size());
+    }
+
+    public Map<Integer, Answer> getAnswerMap() {
+        return answerMap;
+    }
+
+    public void setUserAnswers(Integer ... userAnswer) {
+        for (int i = 0; i < userAnswer.length; i++) {
+            userAnswerList.add(userAnswer[i]);
+        }
+    }
+
+    public boolean isCorrectAnswer() {
+        for (Integer key : answerMap.keySet()) {
+            Answer answer = answerMap.get(key);
+
+            if (answer.isCorrect()) {
+                if (!userAnswerList.contains(key)) {
+                    return false;
+                }
+            } else {
+                if (userAnswerList.contains(key)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
